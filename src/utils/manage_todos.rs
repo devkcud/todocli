3f3dd24dd51e61194073bb759be062843ed8da87
devkcud <andrescalisejr@gmail.com>
2022-12-07 09:13:14 -0300

    [feat] Add create/delete feature

--- src/utils/manage_todos.rs
+++ src/utils/manage_todos.rs
@@ -1,7 +1,11 @@
+use std::fs::write;
+
+use dirs::config_dir;
 use term_grid::{ Grid, GridOptions, Cell };
 
 use crate::utils::functions::todo_to_vec;
 use colored::Colorize;
+use json::object;
 
 pub fn show_todo_list(config: json::JsonValue) {
     let mut grid = Grid::new(GridOptions {
@@ -21,4 +25,45 @@ pub fn show_todo_list(config: json::JsonValue) {
     }
 
     println!("Size: {}", todos.len().to_string().yellow());
+}
+
+pub fn add_todo(config: &json::JsonValue, what_to_do: &str) {
+    let mut todos = todo_to_vec(config.to_owned());
+
+    if what_to_do.trim().is_empty() {
+        return;
+    }
+
+    todos.push(what_to_do.to_string());
+
+    let obj = object! { "todos": todos };
+    let cfg_file = format!("{}/todocli/config.json", config_dir().unwrap().to_str().unwrap());
+    write(cfg_file, obj.to_string()).unwrap();
+}
+
+pub fn remove_todo(config: json::JsonValue, index: &str) {
+    let mut todos = todo_to_vec(config);
+
+    let index: usize = index
+        .parse::<i32>()
+        .unwrap()
+        .max(0)
+        .min(i32::try_from(todos.len() - 1).unwrap())
+        .try_into()
+        .expect("index out of bounds");
+
+    todos.get(index).expect("Out of bounds");
+
+    todos.remove(index);
+
+    let obj = object! { "todos": todos };
+    let cfg_file = format!("{}/todocli/config.json", config_dir().unwrap().to_str().unwrap());
+    write(cfg_file, obj.to_string()).unwrap();
+}
+
+pub fn reset_todo_tree() {
+    let v: Vec<String> = vec![];
+    let obj = object! { "todos": v };
+    let cfg_file = format!("{}/todocli/config.json", config_dir().unwrap().to_str().unwrap());
+    write(cfg_file, obj.to_string()).unwrap();
 }
