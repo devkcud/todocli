15f37051868134b6e71aa9eca3348cd4f4b729b9
devkcud <andrescalisejr@gmail.com>
2022-12-07 05:36:55 -0300

    [feat] Change from YAML to JSON

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,60 @@
-use std::{path::Path, fs, process::exit};
+use std::{ path::Path, fs::{ create_dir, read_to_string, write }, process::exit };
 
 use dirs::config_dir;
-use yaml_rust::{YamlLoader, Yaml};
-use term_grid::{GridOptions, Grid, Direction, Cell};
+use json;
+use term_grid::{ Grid, GridOptions, Cell };
 use colored::Colorize;
 
 fn main() {
-    let mut config: Vec<Yaml> = vec![];
+    let mut config: json::JsonValue = json::JsonValue::Null;
     load_configuration(&mut config);
-    let config = &config[0];
 
     // Check if config `todos` exists
-    if config["todos"].is_badvalue() == true {
-        println!("The configuration file's to-do list section is missing.");
+    if config["todos"].is_null() == true {
+        println!("No todo list found in the configuration file.");
         exit(1);
     }
 
-    show_todo_list(&config);
+    show_todo_list(config);
 }
 
-fn load_configuration(config: &mut Vec<Yaml>) {
+fn load_configuration(config: &mut json::JsonValue) {
     // Getting the config file/folder
-    let config_dir = format!("{}/todocli", config_dir().unwrap().to_str().unwrap().replace("\\", "/"));
-    let config_file = format!("{}/config.yml", config_dir);
+    let config_dir = format!("{}/todocli", config_dir().unwrap().to_str().unwrap());
+    let config_file = format!("{}/config.json", config_dir);
 
-    if Path::new(&config_dir).is_dir()   == false { fs::create_dir(&config_dir).expect("Error creating config folder."); }
-    if Path::new(&config_file).is_file() == false { fs::write(&config_file, "todos: []").expect("Error creating config file."); }
-
+    (!Path::new(&config_dir).is_dir()).then(|| create_dir(&config_dir));
+    (Path::new(&config_file).is_file() == false).then(|| write(&config_file, "{ \"todos\": [] }"));
     // It's loading the configuration file into the `config` variable.
-    *config = YamlLoader::load_from_str(&fs::read_to_string(config_file).unwrap()).unwrap();
+    *config = json::parse(&read_to_string(config_file).unwrap()).unwrap();
 }
 
-fn show_todo_list(config: &Yaml) {
-    let mut output_grid = Grid::new(GridOptions { direction: Direction::LeftToRight, filling: term_grid::Filling::Spaces(1) });
+fn todos_to_vec(config: json::JsonValue) -> Vec<String> {
+    let mut list: Vec<String> = vec![];
+    let mut todos = config["todos"].clone();
 
-    // Iterating over the todos in the config file and adding them to the output_grid.
-    for (i, todo) in config["todos"].as_vec().unwrap().iter().enumerate() {
-        output_grid.add(Cell::from(i.to_string().yellow().to_string()));
-        output_grid.add(Cell::from(todo.as_str().unwrap().blue().to_string()));
+    // It's iterating over the `todos` array and pushing each item into the `list` vector.
+    (0..todos.len()).for_each(|i| list.push(todos[i].take_string().unwrap()));
+
+    return list;
+}
+
+fn show_todo_list(config: json::JsonValue) {
+    let mut grid = Grid::new(GridOptions {
+        direction: term_grid::Direction::LeftToRight,
+        filling: term_grid::Filling::Spaces(1),
+    });
+
+    let todos = todos_to_vec(config);
+
+    for (i, todo) in todos.iter().enumerate() {
+        grid.add(Cell::from(i.to_string().yellow().to_string()));
+        grid.add(Cell::from(todo.to_string().blue().to_string()));
     }
 
-    let todo_size = config["todos"].as_vec().unwrap().len();
+    if todos.len() > 0 {
+        println!("{}\n", grid.fit_into_columns(2).to_string().trim());
+    }
 
-    if todo_size == 0 { return; }
-
-    println!("{}", output_grid.fit_into_columns(2).to_string().trim());
-    println!("\nQuantity of items: {}", todo_size.to_string().yellow().to_string());
+    println!("Size: {}", todos.len().to_string().yellow());
 }
